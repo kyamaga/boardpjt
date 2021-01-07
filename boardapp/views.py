887523caba2995e2c178f1cc04aad5fc4e017a94
kyamaga <yamaga@shikoku-gas.co.jp>
2021-01-07 12:35:43 +0900

    会社20210107

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
+from .models import BoardModel
 
 # Create your views here.
 
@@ -13,3 +15,19 @@ def signupfunc(request):
         except IntegrityError:
             return render(request, 'signup.html', {'error':'このユーザーはすでに登録されています。'})
     return render(request, 'signup.html', {})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {'context':'loged in'})
+        else:
+            return render(request, 'login.html', {'context':'not loged in'})
+    return render(request, 'login.html', {'context':'get method'})
+
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list':object_list})
