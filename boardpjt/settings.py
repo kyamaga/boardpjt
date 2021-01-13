bb8d1ee9baac9e4e8f0d659584ba4a9a01ed3dba
kyamaga <yamaga@shikoku-gas.co.jp>
2021-01-13 21:25:30 +0900

    自宅20210113

--- boardpjt/settings.py
+++ boardpjt/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/sta/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 MEDIA_URL = 'medi/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
